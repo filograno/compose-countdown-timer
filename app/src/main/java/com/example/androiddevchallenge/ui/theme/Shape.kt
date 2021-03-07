0d3d5c0bf41ee0a13fa1c7ac573e623ed722f550
Giuseppe Filograno <filograno@gmail.com>
2021-03-07 19:22:55 +0100

    Change buttons shape

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Shape.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Shape.kt
@@ -15,12 +15,13 @@
  */
 package com.example.androiddevchallenge.ui.theme
 
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
+    small = CircleShape,
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
